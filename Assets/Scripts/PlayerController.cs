be460935d0e893f4e95a7da80872c0b4f6c4cf78
ranthilina99 <umeshranthilina111@gmail.com>
2021-05-04 10:10:04 +0530

    3D game design unity

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //private variable
+    private float speed = 15.0f;
+
+    private float turnSpeed = 25.0f;
+
+    private float horizontalInput;
+
+    private float forwardInput;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //This is where we get input player
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+        
+        // we'll move the vehicle forward
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        
+        //right and left vehicle position not turn vehicle
+        //transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
+        
+        //we turn the vehicle
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
+    }
+}
